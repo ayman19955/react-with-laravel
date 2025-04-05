1ca2e245813e3cfa7d33eb70af72e4a3e4dc7d60
ayman19955 <abuobeid42@gmail.com>
2025-04-05 17:23:38 +0300

    COMPLETE OVERWRITE: Latest code from local machine

--- react/src/views/users.jsx
+++ react/src/views/users.jsx
@@ -1,34 +1,60 @@
 import { useEffect, useState } from "react"
 import axiosClient from "../axios-client"
 import { Link } from "react-router-dom";
+
 export default function users() {
-  const {users,setUsers} = useState([])
-  const [loading ,setLoading]  = useState(false)
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-
-
     getUsers();
   }, []);
-  const getUsers=()=>{
+  const getUsers = () => {
     setLoading(true)
     axiosClient.get('/users')
-    .then(({data})=>{
-      setLoading(false)
-    })
-    .catch(()=>{
-      setLoading(false)
-    })
+      .then(({ data }) => {
+        setUsers(data.data)
+        setLoading(false)
+      })
+      .catch(() => {
+        setLoading(false)
+      })
   }
   return (
-      <div>
-          <div style={{ display: 'flex', justifyContent: 'space-between' ,alignItems: 'center'}}>
-            <h1>Users</h1>
-            <Link to={'/users/new'} className="btn-add">Add new</Link>
-          </div>
-          <div className="card animated fadeInDown">
-            <table ></table>
-          </div>
+    <div>
+      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <h1>Users</h1>
+        <Link to={'/users/new'} className="btn-add">Add new</Link>
       </div>
+      <div className="card animated fadeInDown">
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Created at</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {loading && <div>Loading users...</div>}
+            {users.map(u => (
+              <tr key={u.id}>
+
+                <td>{u.id}</td>
+                <td>{u.name}</td>
+                <td>{u.email}</td>
+                <td>{u.created_ad}</td>
+                <td>
+                  <Link className="btn-edit" to={'/users/' + u.id}>Edit</Link>
+                  <button className="btn-delete">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
